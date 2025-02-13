369addb1abcc097194b71a3131d5605c3968cf6f
Ghozy165 <85822663+Ghozy165@users.noreply.github.com>
2025-02-13 18:57:57 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import './styles/App.css'
 import Navbar from "./components/Navbar";
+import ProductDetail from "./components/ProductDetail";
 import ProductPicture from "./components/ProductPicture";
 
 export default function App() {
@@ -8,29 +9,7 @@ export default function App() {
       <Navbar />
       <main className="container">
         <ProductPicture />
-        <div className="product-detail">
-          <h2 className="store-name">Sneaker Company</h2>
-          <h1 className="product-name">Fall Limited Edition Sneakers</h1>
-          <p className="product-description">
-            These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they’ll withstand everything
-            the weather can offer.
-          </p>
-          <div className="price">
-            <div >
-              <h3 className="discounted-price">$125.00<span className="discount">50%</span></h3>
-              <h4 className="real-price">$250.00</h4>
-            </div>
-          </div>
-          <div className="cart">
-            <div className="cart-quantity">
-              <button className="minus">-</button>
-              <span>0</span>
-              <button className="plus">+</button>
-            </div>
-            <button className="add-to-cart">Add to cart</button>
-          </div>
-        </div>
+        <ProductDetail />
       </main>
     </>
   );
---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,31 @@
+import "./ProductDetail.css"
+
+export default function ProductDetail() {
+  return (
+    <div className="product-detail">
+      <h2 className="store-name">Sneaker Company</h2>
+      <h1 className="product-name">Fall Limited Edition Sneakers</h1>
+      <p className="product-description">
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
+      </p>
+      <div className="price">
+        <div>
+          <h3 className="discounted-price">
+            $125.00<span className="discount">50%</span>
+          </h3>
+          <h4 className="real-price">$250.00</h4>
+        </div>
+      </div>
+      <div className="cart">
+        <div className="cart-quantity">
+          <button className="minus">-</button>
+          <span>0</span>
+          <button className="plus">+</button>
+        </div>
+        <button className="add-to-cart">Add to cart</button>
+      </div>
+    </div>
+  );
+}
