5683e04555492df65ab6ea3e3eeeaedfc1b9e663
Ghozy165 <85822663+Ghozy165@users.noreply.github.com>
2025-02-13 12:12:10 +0700

    feat: product carousel

--- src/components/ProductPicture.jsx
+++ src/components/ProductPicture.jsx
@@ -1,109 +1,182 @@
-import productImage1 from "../assets/images/image-product-1.jpg";
-import productImage2 from "../assets/images/image-product-2.jpg";
-import productImage3 from "../assets/images/image-product-3.jpg";
-import productImage4 from "../assets/images/image-product-4.jpg";
-
-import productImageThumbnail1 from "../assets/images/image-product-1-thumbnail.jpg";
-import productImageThumbnail2 from "../assets/images/image-product-2-thumbnail.jpg";
-import productImageThumbnail3 from "../assets/images/image-product-3-thumbnail.jpg";
-import productImageThumbnail4 from "../assets/images/image-product-4-thumbnail.jpg";
-
+import { useState } from "react";
 import "./ProductPicture.css";
 
 import nextIcon from "../assets/images/icon-next.svg";
 import prevIcon from "../assets/images/icon-previous.svg";
 
-import { useState } from "react";
+const images = import.meta.glob("../assets/images/*.jpg", {
+  eager: true,
+  import: "default",
+});
+
+// Menyusun array gambar dengan akses yang lebih aman
+const productImages = [
+  {
+    full: images["../assets/images/image-product-1.jpg"] || "",
+    thumb: images["../assets/images/image-product-1-thumbnail.jpg"] || "",
+  },
+  {
+    full: images["../assets/images/image-product-2.jpg"] || "",
+    thumb: images["../assets/images/image-product-2-thumbnail.jpg"] || "",
+  },
+  {
+    full: images["../assets/images/image-product-3.jpg"] || "",
+    thumb: images["../assets/images/image-product-3-thumbnail.jpg"] || "",
+  },
+  {
+    full: images["../assets/images/image-product-4.jpg"] || "",
+    thumb: images["../assets/images/image-product-4-thumbnail.jpg"] || "",
+  },
+];
 
 export default function ProductPicture() {
   const [isActive, setIsActive] = useState(false);
-  const handleToggle = () => setIsActive(!isActive);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleToggle = () => {
+    if (window.innerWidth > 768) {
+      setIsActive(!isActive);
+    }
+  };
+
+  const handleNext = () =>
+    setCurrentIndex((prev) => (prev + 1) % productImages.length);
+
+  const handlePrev = () =>
+    setCurrentIndex(
+      (prev) => (prev - 1 + productImages.length) % productImages.length
+    );
+
+  const handleThumbnailClick = (index) => setCurrentIndex(index);
 
   return (
     <>
-      <div
-        className={`overlay ${isActive ? "overlay-active" : ""}`}
-        onClick={(e) => {
-          e.stopPropagation();
-          handleToggle();
-        }}
-      ></div>
+      {/* Overlay */}
+      {isActive && (
+        <div className="overlay overlay-active" onClick={handleToggle}></div>
+      )}
+
+      {/* Carousel utama */}
       <div className="product-carousel">
         <div className="product-carousel-image" onClick={handleToggle}>
-          <picture className="product-carousel-item isActive">
-            <img src={productImage1} alt="" />
-          </picture>
           <picture className="product-carousel-item">
-            <img src={productImage2} alt="" />
+            <img
+              src={productImages[currentIndex].full}
+              alt={`Product ${currentIndex + 1}`}
+            />
           </picture>
-          <picture className="product-carousel-item">
-            <img src={productImage3} alt="" />
-          </picture>
-          <picture className="product-carousel-item">
-            <img src={productImage4} alt="" />
-          </picture>
-          <button className="prev-control">
-            <img src={prevIcon} alt="" />
+
+          {/* Tombol Navigasi */}
+          <button className="prev-control" onClick={handlePrev}>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M11 1 3 9l8 8"
+                stroke="#1D2026"
+                strokeWidth="3"
+                fill="none"
+                fillRule="evenodd"
+              />
+            </svg>
           </button>
-          <button className="next-control">
-            <img src={nextIcon} alt="" />
+          <button className="next-control" onClick={handleNext}>
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m2 1 8 8-8 8"
+                stroke="#1D2026"
+                strokeWidth="3"
+                fill="none"
+                fillRule="evenodd"
+              />
+            </svg>
           </button>
         </div>
+
+        {/* Thumbnail */}
         <div className="product-carousel-thumbnail">
-          <div className="product-image-thumbnail">
-            <input
-              type="radio"
-              className="radio-thumbnail"
-              id="thumbnail-1"
-              name="thumbnail"
-              value="1"
-            />
-            <label htmlFor="thumbnail-1">
-              <div className="thumbnail-overlay"></div>
-              <img src={productImageThumbnail1} alt="" />
-            </label>
-          </div>
-          <div className="product-image-thumbnail">
-            <input
-              type="radio"
-              className="radio-thumbnail"
-              id="thumbnail-2"
-              name="thumbnail"
-              value="2"
-            />
-            <label htmlFor="thumbnail-2">
-              <div className="thumbnail-overlay"></div>
-              <img src={productImageThumbnail2} alt="" />
-            </label>
-          </div>
-          <div className="product-image-thumbnail">
-            <input
-              type="radio"
-              className="radio-thumbnail"
-              id="thumbnail-3"
-              name="thumbnail"
-              value="3"
-            />
-            <label htmlFor="thumbnail-3">
-              <div className="thumbnail-overlay"></div>
-              <img src={productImageThumbnail3} alt="" />
-            </label>
-          </div>
-          <div className="product-image-thumbnail">
-            <input
-              type="radio"
-              className="radio-thumbnail"
-              id="thumbnail-4"
-              name="thumbnail"
-              value="4"
-            />
-            <label htmlFor="thumbnail-4">
-              <div className="thumbnail-overlay"></div>
-              <img src={productImageThumbnail4} alt="" />
-            </label>
-          </div>
+          {productImages.map(({ thumb }, index) => (
+            <div key={index} className="product-image-thumbnail">
+              <input
+                type="radio"
+                className="radio-thumbnail"
+                id={`thumbnail-${index}`}
+                name="thumbnail"
+                checked={currentIndex === index}
+                onChange={() => handleThumbnailClick(index)}
+              />
+              <label htmlFor={`thumbnail-${index}`}>
+                <div
+                  className={`thumbnail-overlay ${
+                    currentIndex === index ? "active" : ""
+                  }`}
+                ></div>
+                <img src={thumb} alt={`Thumbnail ${index + 1}`} />
+              </label>
+            </div>
+          ))}
         </div>
       </div>
+
+      {/* Modal Lightbox */}
+      {isActive && (
+        <div className="product-carousel-modal lightbox lightbox-active">
+          <div className="product-carousel-image">
+            <picture className="product-carousel-item">
+              <img
+                src={productImages[currentIndex].full}
+                alt={`Product ${currentIndex + 1}`}
+              />
+            </picture>
+
+            {/* Tombol Navigasi */}
+            <button className="prev-control" onClick={handlePrev}>
+              <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M11 1 3 9l8 8"
+                  stroke="#1D2026"
+                  strokeWidth="3"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+            <button className="next-control" onClick={handleNext}>
+              <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="m2 1 8 8-8 8"
+                  stroke="#1D2026"
+                  strokeWidth="3"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Thumbnail di Modal */}
+          <div className="product-carousel-thumbnail">
+            {productImages.map(({ thumb }, index) => (
+              <div key={index} className="product-image-thumbnail">
+                <input
+                  type="radio"
+                  className="radio-thumbnail"
+                  id={`modal-thumbnail-${index}`}
+                  name="modal-thumbnail"
+                  checked={currentIndex === index}
+                  onChange={() => handleThumbnailClick(index)}
+                />
+                <label htmlFor={`modal-thumbnail-${index}`}>
+                  <div
+                    className={`thumbnail-overlay ${
+                      currentIndex === index ? "active" : ""
+                    }`}
+                  ></div>
+                  <img src={thumb} alt={`Thumbnail ${index + 1}`} />
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
