9ece22a89d5d5b329ba3cf3eb9b3145443334a7a
Ghozy165 <labib20ti@mahasiswa.pcr.ac.id>
2025-02-15 23:52:49 +0700

    feat: product detail mobile

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,30 +1,50 @@
 import "./ProductDetail.css"
 
+import IconPlus from "../assets/images/icon-plus.svg";
+import IconMinus from "../assets/images/icon-minus.svg";
+import IconCart from "../assets/images/icon-cart.svg";
+
+import { useState } from "react";
+
 export default function ProductDetail() {
+  const [quantity, setQuantity] = useState(0);
   return (
     <div className="product-detail">
-      <h2 className="store-name">Sneaker Company</h2>
-      <h1 className="product-name">Fall Limited Edition Sneakers</h1>
-      <p className="product-description">
-        These low-profile sneakers are your perfect casual wear companion.
-        Featuring a durable rubber outer sole, they’ll withstand everything the
-        weather can offer.
-      </p>
-      <div className="price">
-        <div>
-          <h3 className="discounted-price">
-            $125.00<span className="discount">50%</span>
-          </h3>
-          <h4 className="real-price">$250.00</h4>
-        </div>
+      <div className="product">
+        <h2 className="store-name">Sneaker Company</h2>
+        <h1 className="product-name">Fall Limited Edition Sneakers</h1>
+        <p className="product-description">
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything the
+          weather can offer.
+        </p>
       </div>
-      <div className="cart">
+      <div className="price">
+        <h3 className="discounted-price">
+          $125.00<span className="discount">50%</span>
+        </h3>
+        <h4 className="real-price">$250.00</h4>
+      </div>
+      <div className="add-cart">
         <div className="cart-quantity">
-          <button className="minus">-</button>
-          <span>0</span>
-          <button className="plus">+</button>
+          <button>
+            <img src={IconMinus} alt="minus" />
+          </button>
+          <input
+            type="number"
+            min="1"
+            className="quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+          <button className="plus">
+            <img src={IconPlus} alt="plus" />
+          </button>
         </div>
-        <button className="add-to-cart">Add to cart</button>
+        <button className="btn-add-to-cart">
+          <img src={IconCart} alt="cart" />
+          <span>Add to cart</span>
+        </button>
       </div>
     </div>
   );
